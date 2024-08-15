f1da645979f3eec1127693822c1adc465b29bafc
Sungwoo97 <gwaja97@gmail.com>
2024-08-16 04:12:56 +0900

    slide js 1차 구현

--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
 const slideWrapper = document.querySelector('.slide-wrapper');
-const slideContainer = document.querySelector('.slide-container');
-const slides = document.querySelectorAll('.slide-container li');
-let currentSlide = 0;
-let slidesCount = slides.length;
+const slideContainer = slideWrapper.querySelector('.slide-container');
+const slides = slideContainer.querySelectorAll('.slide-container li');
+let currentIdx = 0;
+const slidesCount = slides.length;
+const prevBtn = slideWrapper.querySelector('.prev');
+const nextBtn = slideWrapper.querySelector('.next');
+let slideTimer;
 
-slideContainer.style.width = `${slideWrapper * slidesCount}px`
+
+
+// let slideWidth = slideWrapper.offsetWidth;
+// let slidesContainerWidth = slideWidth * slidesCount;
+// slideContainer.style.transform = `translateX(-${slidesContainerWidth}px)`;
+// console.log(slideContainer.style.transform);
+
+slides.forEach((slide, idx)=>{
+  slide.style.left = `${idx * 100}%`;
+})
 
 function showSlide(num){
 
+  if(num > slidesCount-1){
+    num = 0;
+  }
+  if(num < 0){
+    num = slidesCount-1;
+  }
+  slideContainer.style.left =`${-num*100}%`;
+  currentIdx = num;
+  
 }
+showSlide(0);
+
+prevBtn.addEventListener('click', ()=>{
+  showSlide(currentIdx - 1);
+});
+
+nextBtn.addEventListener('click', ()=>{
+  showSlide(currentIdx + 1);
+});
+
+function autoSlide(){
+  slideTimer = setInterval(()=>{
+  showSlide(currentIdx + 1);
+  },3000);
+}
+autoSlide();
+
+slideContainer.addEventListener('mouseenter', ()=>{
+  clearInterval(slideTimer);
+})
+slideContainer.addEventListener('mouseleave',()=>{
+  autoSlide();
+})
