52aaa95016f228207276346480b6a773d6c80da4
Sungwoo97 <gwaja97@gmail.com>
2024-08-16 15:37:46 +0900

    slide js 2차 구현

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const slideWrapper = document.querySelector('.slide-wrapper');
-const slideContainer = slideWrapper.querySelector('.slide-container');
+let slideContainer = slideWrapper.querySelector('.slide-container');
 const slides = slideContainer.querySelectorAll('.slide-container li');
 let currentIdx = 0;
 const slidesCount = slides.length;
@@ -7,54 +7,100 @@ const prevBtn = slideWrapper.querySelector('.prev');
 const nextBtn = slideWrapper.querySelector('.next');
 let slideTimer;
 
+for(let i = 0; i<slidesCount;i++){
+  let cloneSlide = slides[i].cloneNode(true); //복사
+  cloneSlide.classList.add('clone'); //복사 클래스 추가
+  slideContainer.appendChild(cloneSlide); //ul의 내용의 뒤에 추가
+}
+for(let i = slidesCount -1; i >=0 ; i--){
+  let cloneSlide = slides[i].cloneNode(true); //복사
+  cloneSlide.classList.add('clone'); //복사 클래스 추가
+  slideContainer.prepend(cloneSlide); //ul의 내용의 앞에 추가
+}
+
+let allslides = slideContainer.querySelectorAll('li'); //복사본 포함 전체 슬라이드
+let allslidesCount = allslides.length;
 
 function setLayout(){
  let slideWidth = slideWrapper.offsetWidth;
  let slidesContainerWidth = slideWidth * slidesCount;
  slideContainer.style.transform = `translateX(-${slidesContainerWidth}px)`;
- console.log(slideContainer.style.transform);
 }
+setLayout();
 
 window.addEventListener('resize',()=>{
   setLayout();
 })
-slides.forEach((slide, idx)=>{
+console.log(allslides[0]);
+
+//연속 클릭 방지
+function debounce(callback, time){
+  let slideTrigger = true;
+  return (e)=>{
+    if(slideTrigger){
+      callback(e);
+      slideTrigger = false;
+      setTimeout(()=>{
+        slideTrigger = true;
+        console.log(slideTrigger);
+      }, time);
+    }
+    console.log(slideTrigger);
+  }
+}
+
+allslides.forEach((slide, idx)=>{
   slide.style.left = `${idx * 100}%`;
 })
+console.log(slides[0]);
 
 function showSlide(num){
-  if(currentIdx === num ) return;
-  if(num > slidesCount - 1){
-    num = 0;
-  }
-  if(num < 0){
-    num = slidesCount - 1;
-  }
-  
   slideContainer.style.left =`${-num*100}%`;
   currentIdx = num;
-  console.log(num);
+  console.log(currentIdx);
+  if(currentIdx === slidesCount * 2 - 1 ){
+    setTimeout(()=>{
+      slideContainer.classList.remove('animated');
+      slideContainer.style.left = `${(slidesCount-1)*-100}%`;
+      currentIdx = slidesCount-1;
+    },400);
+    setTimeout(()=>{
+      slideContainer.classList.add('animated');
+    },500);
+  }
+  if(currentIdx === -3){
+    setTimeout(()=>{
+      slideContainer.classList.remove('animated');
+      slideContainer.style.left = 0;
+       currentIdx = 0;
+    },400);
+    setTimeout(()=>{
+      slideContainer.classList.add('animated');
+    },500);
+    
+  }
+
 }
 showSlide(0);
 
-prevBtn.addEventListener('click', ()=>{
+prevBtn.addEventListener('click', debounce(()=>{
   showSlide(currentIdx - 1);
-});
+}, 500));
 
-nextBtn.addEventListener('click', ()=>{
+nextBtn.addEventListener('click', debounce(()=>{
   showSlide(currentIdx + 1);
-});
+}, 500));
 
 function autoSlide(){
   slideTimer = setInterval(()=>{
-  showSlide(currentIdx + 1);
+  showSlide((currentIdx + 1)%slidesCount);
   },3000);
 }
 autoSlide();
 
 slideWrapper.addEventListener('mouseenter', ()=>{
   clearInterval(slideTimer);
-})
+});
 slideWrapper.addEventListener('mouseleave',()=>{
   autoSlide();
-})
+});
